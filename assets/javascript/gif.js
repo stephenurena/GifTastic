6d13bc4316a1ce6b010d00bf254f68bc72fc750e
Stephen Urena <surena@Stephens-MacBook-Air.local>
2017-02-21 22:14:00 -0800

    able to appen new user inputs for buttons and use data-name for event handler. also onclick btn works to display image/rating, by calling api, need to be able to click new btn

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,15 +1,51 @@
 //variables for input values after event handler, when clicked.
-var searchTerm = encodeURI($("#animalInput").val());
-var limit = encodeURI($("#limit").val()); //maybe just make a static 10 image limit on queryURL
+// var searchTerm = encodeURI($("#animalInput").val());
 // variables for animate and pause gifs
 // var state = $(this).attr("");
 // var still = $(this).attr("");
 // var animate = $(this)attr("");
+//might be redundant but using variables to keep code organized
+
+function displayGifs() {
+	var queryTopics = $(this).attr("data-name");
+	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + queryTopics + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+	$.ajax({
+		url: queryURL,
+	    method: "GET"
+	    }).done(function(response) {
+
+	    	var results = response.data;
+
+	    	for (var i = 0; i < results.length; i++) {
+		    	//creating a div to hold our gifs
+		    	var gifDiv = $("<div class='col-xs-3 col-md-3'>");
+
+		    	var p = $("<p>").text("Rating: " + results[i].rating);
+		    	
+		    	// ==============================================
+		    	//created and storing an image tag;
+		    	var gifImage= $("<img class='thumbnail'>");
+
+		    	//setting the gifImage src attr to imageURL
+		    	gifImage.attr("src", results[i].images.fixed_height.url);
+		    	gifImage.attr("alt", "funny gifs");
+
+		    	//append gifImage to topicsDiv
+		    	gifDiv.append(p);
+		    	gifDiv.append(gifImage);
+		    	//append topicDiv to the topics div
+		    	$("#topics").append(gifDiv);
+	    	}
+
+	    });
+}
+
 function addGifTopics() {
 
 
-$("#animalBtns").empty();
-		
+	$("#topicBtns").empty();
+			
 		//loops through the array of arrTopics
 		for (var i = 0; i < arrTopics.length; i++) {
 		//create var to create element button
@@ -21,53 +57,19 @@ $("#animalBtns").empty();
 		//display arrTopics[i] and capitalize each word on style.css
 		a.text(arrTopics[i]);
 		//add a new button on every user input
-		$("#animalBtns").append(a);
-
-		console.log(arrTopics[i]); //test of arrTopics[i]
+		$("#topicBtns").append(a);
 	}
-};
+}
 
-var arrTopics = [];
+var arrTopics = ["charles barkley", "michael jordan"];
 $("#addTopic").on("click", function(event) {
 	event.preventDefault();
 	var string = $("#addInput").val();
 	arrTopics.push(string);console.log(arrTopics);
 	addGifTopics();
-})
+});
+//add click event to all elements with a class of "gifTopic"
+$(document).on("click", ".gifTopic", displayGifs);
+//calling function to display initial buttons
+addGifTopics();
 
-//might be redundant but using variables to keep code organized
-// var authKey = "dc6zaTOxFJmzC";
-// var limit = "&limit=10"
-// var rating = "&rating=";
-// var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=" + authKey + limit + rating;
-
-// $(".gifTopic").on("click", function(){
-
-// 	$.ajax({
-
-// 		//maybe add an event handler, when an element is clicked do something
-// 		//add possibly another event handler, when an input is submitted do something...add button
-// 		//some type of event handler when button is clicked query searches the value of button
-
-// 	        url: queryURL,
-// 	        method: "GET"
-// 	      })
-// 	.done(function(response) {
-
-// 		//created variable to hold the img url
-// 		var imageURL = response.data.image_original_url;
-
-// 		//created a variable to create img element
-// 		var topicImg = $("<img>");
-
-// 		//add attributes, of src = imageURL and alt text = "funny gif"
-// 		topicImg.attr("src", imageURL);
-// 		topicImg.attr("alt", "funny gif");
-
-// 		//then on a button click, add 10 new images and rating
-// 		$("").append(topicImg); 
-
-// 	      	//append 10 static images together
-
-// 	     });
-// // });
